63e95bf7d233269141faba15a1ce86f3da527a93
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-03-20 15:23:40 +0700

    huy

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:myapp/splash_screen.dart';
 
 class MapSceen extends StatefulWidget {
   @override
@@ -10,39 +11,47 @@ class MapSceen extends StatefulWidget {
 }
 
 class MapSceenState extends State<MapSceen> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Set<Marker> markers = new Set();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Future<Map<String, dynamic>> getPosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    markers.add(Marker( //add second marker
+      markerId: MarkerId("Me"),
+      position: LatLng(position.toJson()['latitude'], position.toJson()['longitude']), //position of marker
+      infoWindow: InfoWindow( //popup info
+        title: 'My Custom Title ',
+        snippet: 'My Custom Subtitle',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+    return position.toJson();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: FutureBuilder(
+        future: getPosition(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container();
+          }
+          if (snapshot.hasData) {
+            return GoogleMap(
+              myLocationButtonEnabled: false,
+              zoomGesturesEnabled: true,
+              markers: markers,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(snapshot.data['latitude'], snapshot.data['longitude']),
+                zoom: 14.4746,
+              ),
+            );
+          }
+          return Container();
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
